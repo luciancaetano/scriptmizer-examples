07bbca751c034d48515c1f22ebb2ba56d43deb98
Lucian Caetano <lucian.santos.caetano@gmail.com>
2020-10-27 01:26:13 -0300

    ⬆️ Update dependencies.

--- scripts/run-ls.ts
+++ scripts/run-ls.ts
@@ -2,11 +2,11 @@ import {
     command, onAction, option, sm,
 } from 'scriptmizer';
 
-command('lsDir', () => {
-    option('-d, --directory', 'Prodive a directory name defaults to __dirname', __dirname);
+command('lsDir', async () => {
+    option('--directory <dir>', 'Prodive a directory name defaults', __dirname);
 
     onAction(async () => {
-        const dir = sm.getOption('d');
+        const dir = sm.getOption('directory').toString();
 
         await sm.shellExec(`ls -ls ${dir}`, { log: false });
     });
